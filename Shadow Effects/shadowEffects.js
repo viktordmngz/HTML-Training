fbdf4227d53a60be2a37cc2cd2bd76c8ae9ba88c
Viktor Dominguez <95234756+viktordmngz@users.noreply.github.com>
2024-11-20 19:39:22 -0500

    Add files via upload

---
+++ Shadow Effects/shadowEffects.js
@@ -0,0 +1,8 @@
+function myFunc() {
+  var x = document.getElementById("price-card-container");
+  if (x.style.display === "block") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "block";
+    }
+}
